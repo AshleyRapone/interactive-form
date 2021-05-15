ea3769cbdcc7bad8bde18f12c9c2608c76722e77
Ashley Rapone <ashleylynnrapone@gmail.com>
2021-05-15 18:35:16 -0400

    form validation

--- js/script.js
+++ js/script.js
@@ -76,3 +76,120 @@ activities.addEventListener('input', (event) => {
    // If an activity is unchecked, the total cost should decrease by that amount.
    // The <p> element with the id of "activity-cost" below the activities section should update to reflect the chosen activities' total cost.
 })
+
+/* 7 */
+/*
+Program the "I'm going to pay with" <select> element to listen for user changes. 
+When a change is detected, hide all payment sections in the form’s UI except the selected one.
+*/
+// The credit card payment option should be selected for the user by default.
+const payment = document.getElementById('payment')
+const paymentOptions = payment.children;
+for (let i = 0; i < paymentOptions.length; i++) {
+  if (paymentOptions[i].getAttribute('value') === 'credit-card') {
+    paymentOptions[i].selected = true;
+  }
+
+}
+
+const creditCardPayment = document.getElementById('credit-card');
+const paypalPayment = document.getElementById('paypal');
+const bitcoinPayment = document.getElementById('bitcoin');
+creditCardPayment.style.display = 'block';
+paypalPayment.style.display = 'none';
+bitcoinPayment.style.display = 'none';
+
+payment.addEventListener('input', (event) => {
+  let paymentChoice = event.target.value;
+  const creditCardPayment = document.getElementById('credit-card');
+  const paypalPayment = document.getElementById('paypal');
+  const bitcoinPayment = document.getElementById('bitcoin');
+  console.log(paymentChoice);
+  if (paymentChoice == 'credit-card') {
+    creditCardPayment.style.display = 'block';
+    paypalPayment.style.display = 'none';
+    bitcoinPayment.style.display = 'none';
+  } else if (paymentChoice == 'paypal') {
+      paypalPayment.style.display = 'block';
+      creditCardPayment.style.display = 'none';
+      bitcoinPayment.style.display = 'none';
+  } else if (paymentChoice == 'bitcoin') {
+      creditCardPayment.style.display = 'none';
+      paypalPayment.style.display = 'none';
+      bitcoinPayment.style.display = 'block';
+  }
+
+})
+
+
+/* 8 */
+//if (email === ''
+function valid_name() {
+  return document.getElementById('name').value !== '';
+}
+
+function valid_email() {
+  const email = document.getElementById('email').value
+  const emailRegex = /\w+@[a-z]+(.com)+/ig
+  return emailRegex.test(email)
+}
+
+function activites_registered() {
+  const activitiesBox = document.getElementById('activities-box').children;
+  count = 0
+  for (let i = 0; i < activitiesBox.length; i++) {
+    if (activitiesBox[i].getElementsByTagName('input')[0].checked === true) {
+      count +=1;
+    }   
+  }
+  return count > 0;
+}
+
+function valid_credit_card() {
+  const creditCardNumber = document.getElementById('cc-num').value;
+  const zipcode = document.getElementById('zip').value;
+  const cvv = document.getElementById('cvv').value;
+  const cvvRegEx = /\d{3}/g;
+  const zipcodeRegEx = /\d{5}/g;
+  const creditCardRegEx = /\d{13,16}/g;
+  let count = 0
+  if (cvvRegEx.test(cvv)) {
+    count +=1;
+  }
+  if (zipcodeRegEx.test(zipcode)) {
+    count +=1;
+  }
+  if (creditCardRegEx.test(creditCardNumber)) {
+    count +=1;
+  }
+  return count === 3;
+}
+
+
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  let count = 0
+  if (!valid_name()) {
+    console.log('no valid name');
+    count +=1;
+  }
+  if (!valid_email()) {
+    console.log('no valid email')
+    count +=1;
+  }
+  if (!activites_registered()) {
+    console.log('no activites registered')
+    count +=1;
+  }
+  if (!valid_credit_card()) {
+    console.log('no valid card')
+    count +=1;
+  }
+  if (count > 0) {
+    event.preventDefault();
+  }
+});
+
+//The "Register for Activities" section must have at least one activity selected.
